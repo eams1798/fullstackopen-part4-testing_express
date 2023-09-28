80df1e9f64d25db75eef680158b6b42f7df76712
eams1798 <eams1798@gmail.com>
2023-09-28 13:34:09 -0400

    it works with the latest version of repo #5

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import blogsRouter from "./controllers/blogs";
 import usersRouter from "./controllers/users";
 import loginRouter from "./controllers/login";
+import testingRouter from "./controllers/testing";
 import middleware from "./utils/middleware";
 import logger from "./utils/logger";
 import mongoose from "mongoose";
@@ -26,6 +27,9 @@ const startApp = async () => {
     app.use("/api/login", loginRouter);
     app.use("/api/users", usersRouter);
     app.use("/api/blogs", blogsRouter);
+    if (process.env.NODE_ENV === "test") {
+      app.use("/api/testing", testingRouter);
+    }
     app.use(middleware.unknownEndpoint);
     app.use(middleware.errorHandler);
 
